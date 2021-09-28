712e85473b2807bf0f6750d2922ec8e3aa02a00b
namig <namig.nurmamedov@yandex.ru>
2021-09-28 15:08:45 +0400

    Cache SlugGenerator in ProductSlug

--- src/Catalog/Domain/Entity/Product/ValueObject/ProductSlug.php
+++ src/Catalog/Domain/Entity/Product/ValueObject/ProductSlug.php
@@ -9,10 +9,21 @@ use App\Shared\Domain\ValueObject\Slug;
 
 final class ProductSlug extends Slug
 {
+    private static ?SlugGenerator $slugGenerator = null;
+
     public static function fromName(ProductName $productName): self
     {
-        $slug = (new SlugGenerator())->generate($productName->value());
+        $slug = self::slugGenerator()->generate($productName->value());
 
         return new self($slug);
+    }
+
+    private static function slugGenerator(): SlugGenerator
+    {
+        if (self::$slugGenerator === null) {
+            self::$slugGenerator = (new SlugGenerator());
+        }
+
+        return self::$slugGenerator;
     }
 }
